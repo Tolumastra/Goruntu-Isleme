659d1cb09e8f811e0ec64220bae08085468c37fb
Tolumastra <149951992+Tolumastra@users.noreply.github.com>
2023-11-18 21:12:53 +0300

    Update main.py

--- Odev/main.py
+++ Odev/main.py
@@ -2,19 +2,15 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Örnek bir gri seviye görüntü oluşturun (örneğin, 0 ile 255 arasında rastgele piksel değerleri)
 image = cv2.imread("dragon.jpg",0)
 
-# Görüntüyü histogramı hesapla
 histogram = np.zeros(256, dtype=int)
 
-# Görüntüdeki her pikseli dolaşarak histogramı hesapla
 for i in range(image.shape[0]):
     for j in range(image.shape[1]):
         pixel_value = image[i, j]
         histogram[pixel_value] += 1
 
-# Histogramı görselleştirin
 plt.bar(range(256), histogram, width=1, color='gray')
 plt.title("Gri Seviye Görüntü Histogramı")
 plt.xlabel("Piksel Değeri")
